033a3333468c42661b091f5a93b047d39b799c85
santerilukka <156800715+santerilukka@users.noreply.github.com>
2025-04-21 15:39:44 +0300

    Add ChartView and ColumnSelectionPanel classes for chart display and column selection

--- app/src/main/java/datavisualizer/view/ChartView.java
+++ app/src/main/java/datavisualizer/view/ChartView.java
@@ -1,5 +1,27 @@
 package datavisualizer.view;
 
+import javafx.scene.Node;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
+
 public class ChartView {
-    // TODO: Responsible for displaying chart component
+    private StackPane chartArea;
+
+    public ChartView() {
+        chartArea = new StackPane();
+        chartArea.setPrefSize(600, 400);
+
+        // Default message when no data is loaded
+        Text placeholderText = new Text("Open a CSV file to display chart");
+        chartArea.getChildren().add(placeholderText);
+    }
+
+    public void displayChart(Node chartNode) {
+        chartArea.getChildren().clear();
+        chartArea.getChildren().add(chartNode);
+    }
+
+    public Node getNode() {
+        return chartArea;
+    }
 }
--- app/src/main/java/datavisualizer/view/ColumnSelectionPanel.java
+++ app/src/main/java/datavisualizer/view/ColumnSelectionPanel.java
@@ -1,5 +1,49 @@
 package datavisualizer.view;
 
+import datavisualizer.controller.AppController;
+import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TitledPane;
+import javafx.scene.layout.VBox;
+
 public class ColumnSelectionPanel {
-    // TODO: Responsible for column selection UI
+    private VBox panelRoot;
+    private ListView<String> columnList;
+    private AppController controller;
+    
+    public ColumnSelectionPanel(AppController controller) {
+        this.controller = controller;
+        
+        panelRoot = new VBox(10);
+        panelRoot.setPrefWidth(200);
+        
+        Label titleLabel = new Label("Select Columns");
+        columnList = new ListView<>();
+        
+        // Initially empty - will be populated when data is loaded
+        TitledPane selectionPane = new TitledPane("Available Columns", columnList);
+        selectionPane.setCollapsible(false);
+        selectionPane.setMaxHeight(Double.MAX_VALUE);
+        
+        panelRoot.getChildren().addAll(titleLabel, selectionPane);
+    }
+    
+    public void setColumns(String[] columns) {
+        columnList.getItems().clear();
+        columnList.getItems().addAll(columns);
+        
+        // Set up selection change listener
+        columnList.getSelectionModel().selectedItemProperty().addListener(
+            (obs, oldSelection, newSelection) -> {
+                if (newSelection != null) {
+                    controller.columnSelectionChanged(newSelection);
+                }
+            }
+        );
+    }
+    
+    public Node getNode() {
+        return panelRoot;
+    }
 }
